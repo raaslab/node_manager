157fe5344b7dd5fd4cb893a1a8bf75165bef14e6
Yoonchang Sung <yooncs8@vt.edu>
2018-04-18 22:01:33 -0400

    Put loginfo in node_manager.py

--- scripts/node_manager.py
+++ scripts/node_manager.py
@@ -3,6 +3,7 @@ import rospy
 import time 
 import os 
 from rospy.msg import AnyMsg
+from std_msgs.msg import Int32
 
 lasttime = 0
 
@@ -14,8 +15,9 @@ def listener():
 
 	rospy.init_node('node_manager', anonymous=True)
 	timeout = rospy.get_param('~timeout',2)
-	subtopic = rospy.get_param('~subscribe_to_topic','chatter');
-	nodename = rospy.get_param('~node_name','chatter');
+	subtopic = rospy.get_param('~subscribe_to_topic','chatter')
+	nodename = rospy.get_param('~node_name','chatter')
+	killpub = rospy.Publisher('node_manager/camera_kill',Int32,queue_size=10)
 
 	rospy.loginfo('Subscribing to %s' % subtopic);
 	rospy.loginfo('Node that will be managed: %s' % nodename);
@@ -31,10 +33,17 @@ def listener():
 
 		if (now.secs - lasttime.secs > timeout):
 			lasttime = now
-			rospy.loginfo("Timeout occured")
+			rospy.loginfo("node_manager: timeout occurred at %i" % now.secs)
 			nodes = os.popen("rosnode list").readlines()
 			if any(nodename in s for s in nodes):
+				rospy.loginfo("node_manager: camera_node kill occurred at %i" % now.secs)
+				killpub.publish(1)
 				os.system("rosnode kill " + nodename)
+		else:
+			nodes = os.popen("rosnode list").readlines()
+			if any(nodename in s for s in nodes):
+				rospy.loginfo("node_manager: camera node is there at %i" % now.secs)
+				killpub.publish(0)
 
     		r.sleep()
 
